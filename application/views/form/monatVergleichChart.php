e7e5fb13cdfe13344f19d4cf39a9a7ec3f1bcbdc
slayerrensky <rensky@gmx.de>
2013-04-05 10:02:16 +0200

    dsdfsdf

--- application/views/form/monatVergleichChart.php
+++ application/views/form/monatVergleichChart.php
@@ -55,7 +55,7 @@ $("#container").append('<p><img src="<?php echo base_url(); ?>/img/ajax-loader.g
 		            
 		            for (var k=0,l = daten.length; k<l; k++)
 		            {
-		            	if (gets[i].Unit.contains("W"))
+		            	if (gets[i]["Unit"].indexOf('W') >= 0)
 		            	{
 		            		gets[i].arbeit+=daten[k].Value/12;
 		            	}
@@ -103,7 +103,15 @@ $("#container").append('<p><img src="<?php echo base_url(); ?>/img/ajax-loader.g
 	        }
 	    },
 	    navigator: {
-	    	top: chartSize.navigatorTop
+	    	top: chartSize.navigatorTop,
+	    	xAxis: {
+	    		dateTimeLabelFormats: { // don't display the dummy year
+                hour: '%e. %H:%M',
+                day: '%e',
+                month: '%e. %H:%M',
+                year: '%Y'
+            	}
+          },
 	    },
 		rangeSelector: {
 			selected: 0,
